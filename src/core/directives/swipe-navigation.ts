579417899c5c966d93ee61ba28bb4378efa9dede
Noel De Martin <noel@moodle.com>
2022-03-02 17:55:08 +0100

    wip implement swipe navigation tour

--- src/core/directives/swipe-navigation.ts
+++ src/core/directives/swipe-navigation.ts
@@ -14,6 +14,8 @@
 
 import {  AfterViewInit, Directive, ElementRef, Input, OnDestroy } from '@angular/core';
 import { CoreSwipeNavigationItemsManager } from '@classes/items-management/swipe-navigation-items-manager';
+import { CoreSwipeNavigationTourComponent } from '@components/swipe-navigation-tour/swipe-navigation-tour';
+import { CoreUserTours } from '@features/user-tours/services/user-tours';
 import { Gesture, GestureDetail } from '@ionic/angular';
 import { CoreScreen } from '@services/screen';
 import { GestureController } from '@singletons';
@@ -50,7 +52,8 @@ export class CoreSwipeNavigationDirective implements AfterViewInit, OnDestroy {
     /**
      * @inheritdoc
      */
-    ngAfterViewInit(): void {
+    async ngAfterViewInit(): Promise<void> {
+        // Set up gesture listener
         const style = this.element.style;
         this.swipeGesture = GestureController.create({
             el: this.element,
@@ -69,6 +72,24 @@ export class CoreSwipeNavigationDirective implements AfterViewInit, OnDestroy {
             },
         });
         this.swipeGesture.enable();
+
+        // Show user tour.
+        const source = this.manager?.getSource();
+
+        if (!source) {
+            return;
+        }
+
+        await source.waitForLoaded();
+
+        if ((source.getItems() ?? [])?.length < 2 || !(await CoreUserTours.shouldShow('swipe-navigation'))) {
+            return;
+        }
+
+        await CoreUserTours.show({
+            id: 'swipe-navigation',
+            component: CoreSwipeNavigationTourComponent,
+        });
     }
 
     /**
