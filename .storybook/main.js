94307259de6a337c3238f4e0f2ff9228879caf7f
Noel De Martin <noel@moodle.com>
2022-05-05 17:21:01 +0200

    Disable Storybook actions tab

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 module.exports = {
     framework: '@storybook/angular',
     addons: [
-        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-essentials',
+            options: { actions: false },
+        },
         '@storybook/addon-jest',
     ],
     stories: ['../src/**/*.stories.ts'],
