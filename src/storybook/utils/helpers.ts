b2d744a96615a4fa0325c1b12b852ff6237360eb
Noel De Martin <noel@moodle.com>
2022-05-11 14:51:58 +0200

    MOBILE-3321 core: Configure storybook

---
+++ src/storybook/utils/helpers.ts
@@ -0,0 +1,50 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Meta, Story } from '@storybook/angular';
+
+/**
+ * Define story metadata.
+ *
+ * @param meta Story metadata.
+ * @returns Story metadata.
+ */
+export function meta(meta: Meta): Meta {
+    return meta;
+}
+
+/**
+ * Define story.
+ *
+ * @param template Story template.
+ * @param defaultArgs Default arguments.
+ * @returns Story.
+ */
+export function story<T>(template: Story<T>, defaultArgs: Partial<T> = {}): Story<T> {
+    const story = template.bind({});
+
+    story.args = defaultArgs;
+
+    return story;
+}
+
+/**
+ * Define story template.
+ *
+ * @param template Story template.
+ * @returns Story template.
+ */
+export function template<T>(template: Story<T>): Story<T> {
+    return template;
+}
