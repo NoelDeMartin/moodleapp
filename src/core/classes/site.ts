73fba6a99dc2456dff5306288e6692a63c3fe11d
Noel De Martin <noel@moodle.com>
2022-10-06 15:44:13 +0200

    MOBILE-4059 core: Contact support from errors

--- src/core/classes/site.ts
+++ src/core/classes/site.ts
@@ -1646,6 +1646,10 @@ export class CoreSite {
      */
     async getPublicConfig(options: { readingStrategy?: CoreSitesReadingStrategy } = {}): Promise<CoreSitePublicConfigResponse> {
         if (!this.db) {
+            if (options.readingStrategy === CoreSitesReadingStrategy.ONLY_CACHE) {
+                throw new CoreError('Cache not available to read public config');
+            }
+
             return this.requestPublicConfig();
         }
 
