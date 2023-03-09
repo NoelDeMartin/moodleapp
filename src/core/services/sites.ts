dd3a2b7138698cabfcef3d9c83fd3e40d69cf452
Noel De Martin <noel@moodle.com>
2023-03-09 17:50:44 +0100

    MOBILE-4270 core: Report login errors

--- src/core/services/sites.ts
+++ src/core/services/sites.ts
@@ -509,19 +509,21 @@ export class CoreSitesProvider {
         try {
             data = await Http.post(loginUrl, params).pipe(timeout(CoreWS.getRequestTimeout())).toPromise();
         } catch (error) {
-            throw new CoreError(
-                this.isLoggedIn()
-                    ? Translate.instant('core.siteunavailablehelp', { site: this.currentSite?.siteUrl })
-                    : Translate.instant('core.sitenotfoundhelp'),
-            );
+            throw this.createCannotConnectLoginError(siteUrl, {
+                debug: {
+                    code: 'logintokenerror',
+                    details: error.message,
+                },
+            });
         }
 
         if (data === undefined) {
-            throw new CoreError(
-                this.isLoggedIn()
-                    ? Translate.instant('core.siteunavailablehelp', { site: this.currentSite?.siteUrl })
-                    : Translate.instant('core.sitenotfoundhelp'),
-            );
+            throw this.createCannotConnectLoginError(siteUrl, {
+                debug: {
+                    code: 'logintokenempty',
+                    details: 'The request to /login/token.php returned an empty response',
+                },
+            });
         }
 
         if (data.token !== undefined) {
